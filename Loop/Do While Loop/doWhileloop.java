8e686eb5b1d3b84e67e8b6b5c4d54f9aba1c182f
Satakshi Shanvi <81086303+shanvii@users.noreply.github.com>
2023-12-16 20:41:33 +0530

    Create doWhileloop.java

---
+++ Loop/Do While Loop/doWhileloop.java
@@ -0,0 +1,14 @@
+public class doWhileloop {
+    
+    public static void main(String[] args) 
+    {
+
+        int i = 1;
+
+        do{
+            System.out.println(i + " * " + i + " = " + (i*i));
+            i++;
+        }
+        while(i<=5);
+    }
+}
