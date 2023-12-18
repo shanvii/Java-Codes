c4428054ece965b1fa6983f2d988b11c5372a62b
Satakshi Shanvi <81086303+shanvii@users.noreply.github.com>
2023-12-18 23:25:52 +0530

    Create printingChar.java

---
+++ Unicode/printingChar.java
@@ -0,0 +1,8 @@
+public class printingChar {
+    public static void main(String[] args) {
+        for(int i=2300; i<=65536; i++)
+        {
+            System.out.println(i + " -> " + (char)i);
+        }
+    }
+}
