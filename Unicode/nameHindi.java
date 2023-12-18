6420ea61ba3efffc4ab82773a12f1e2fab6b572e
Satakshi Shanvi <81086303+shanvii@users.noreply.github.com>
2023-12-18 23:22:55 +0530

    Create nameHindi.java

---
+++ Unicode/nameHindi.java
@@ -0,0 +1,33 @@
+public class nameHindi {
+    public static void main(String[] args) {
+        
+        // char name = char(2358) + char(2340) + char(2392) + char(2358) + char(2368);
+        
+            // System.out.println((char)2358 + (char)2340 + (char)2392 + (char)2358 + (char)2368);
+            
+            int[] name = {2358, 2366, 2340, 2392, 2358, 2368};
+            for(int i=0; i<=5; i++)
+            {
+                System.out.print((char)name[i]);
+            }
+            
+            // int i = 2358;
+            // System.out.print((char)i);
+            
+            // i = 2366;
+            // System.out.print((char)i);
+            
+            // i = 2340;
+            // System.out.print((char)i);
+
+            // i = 2392;
+            // System.out.print((char)i);
+
+            // i = 2358;
+            // System.out.print((char)i);
+
+            // i = 2368;
+            // System.out.print((char)i);
+
+    }
+}
